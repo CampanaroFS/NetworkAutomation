c3488aa50f3d035a0f04840a22764d71bee087db
Felipe <felipe.s.campanaro@gmail.com>
2024-02-23 18:35:23 -0300

    Adding my jinja task

---
+++ tasks/02/push_config.py
@@ -0,0 +1,33 @@
+import json
+from jinja2 import Template, Environment, FileSystemLoader
+
+def get_jsonfile():
+    with open('interfaces.json', 'r') as file:
+        int = json.load(file)
+    return int
+
+def get_template():
+    with open('interfaces_felipe.j2', 'r') as template:
+        file = template.read()
+
+    return file
+
+def render_jinja(interface, template):
+    int_template = Template(template)
+    all_int = int_template.render(interfaces=interface)
+
+    return all_int
+
+def push_config(interfaces):
+    with open('interfaces_felipe.txt', 'w') as file:
+        file.write(interfaces)
+         
+
+if __name__ == "__main__":
+    interfaces = get_jsonfile()
+    template = get_template()
+    all_int = render_jinja(interfaces, template)
+    push_config(all_int)
+
+   
+    
